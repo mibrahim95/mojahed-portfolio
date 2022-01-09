c00d176d2a554389ac81596e00a58987529fead3
Mojahed Ibrahim <mojahedibrahim95@gmail.com>
2022-01-09 16:39:18 -0500

    start of contact me section

---
+++ src/components/contactMe.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { Grid, Form } from "semantic-ui-react";
+
+class ContactMe extends Component {
+  render() {
+    return (
+      <Grid.Row id="contactMe" relaxed='very'>
+        <Grid.Column width={6} textAlign='left'>
+          <Form size="big">
+            <Form.Field>
+              <Form.Input
+                label="Full Name"
+                placeholder="Full name"
+                width={12}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Form.Input label="Email" placeholder="Email" width={12} />
+            </Form.Field>
+          </Form>
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <Form size="big">
+            <Form.Group>
+              <Form.TextArea
+                width={10}
+                rows={5}
+                label="Message"
+                placeholder="Please let me know your thought"
+              />
+              <Form.Button>Send</Form.Button>
+            </Form.Group>
+          </Form>
+        </Grid.Column>
+      </Grid.Row>
+    );
+  }
+}
+export default ContactMe;
